8ed32cdda89a265ecd557865ac7821984f5fd908
TITOMWAS <titusmwangi536@gmail.com>
2026-03-11 00:50:42 +0300

    Sync local VS Code changes to GitHub

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "postgres",
-  database: process.env.DB_NAME || "schooldb",
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
